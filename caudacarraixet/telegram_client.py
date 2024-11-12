ddac3b727db660fcd22020e9cdd74a80ec7e3f71
Nacho Borrás <nacho.borras@nexum.com>
2024-11-12 09:12:43 +0100

    Format telegram client

--- caudacarraixet/telegram_client.py
+++ caudacarraixet/telegram_client.py
@@ -11,8 +11,10 @@ class TelegramClient:
 
     def send_message(self, text):
 
-        print(f"Sending message: {text}")
-        url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={text}"
+        message = self._prepare_message(text)
+
+        print(f"Sending message: {message}")
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage?chat_id={self.chat_id}&text={message}"
         response = requests.request("GET", url)
 
         if response.json()['ok'] is False:
@@ -21,6 +23,22 @@ class TelegramClient:
             return None
         print(response.json()['result'])
         return response.json()['result']
+
+    def _prepare_message(self, data):
+        description = data['description']
+        dimension = data['dimension']
+        current_flow_value = data['current_flow_value']
+        current_flow_time = data['current_flow_time']
+        flow_trend = data['flow_trend']
+        max_carraixet_flow = data['max_carraixet_flow']
+
+        message = f"{description} ({dimension})\n" \
+                  f"Valor actual: {current_flow_value} m3/s\n" \
+                  f"Última actualización: {current_flow_time}\n" \
+                  f"Tendencia: {flow_trend}\n" \
+                  f"Límite máximo: {max_carraixet_flow} m3/s"
+
+        return message
 
 
 if __name__ == '__main__':
